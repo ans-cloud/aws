17a2a244287ab59d333e85c60e3d0cf98df68d0e
Nathan Gaskill <21128751+ans-cloud@users.noreply.github.com>
2018-02-01 22:13:08 +0000

    comple

--- LogicMonitor-Collector/CreateLMCollector.py
+++ LogicMonitor-Collector/CreateLMCollector.py
@@ -239,7 +239,7 @@ def updateCollectorDeviceGroup(deviceId,deviceGroupId):
     httpVerb = 'PUT'
     resourcePath = '/device/devices/' + str (deviceId) + '/properties/hostGroupIds'
     queryParams = ''
-    data = '{"value":"' + str (deviceGroupId) + ''"}'
+    data = '{"value":"' + str (deviceGroupId) + '"}'
 
     #Construct URL
     url = 'https://'+ lmHost +'.logicmonitor.com/santaba/rest' + resourcePath + queryParams
